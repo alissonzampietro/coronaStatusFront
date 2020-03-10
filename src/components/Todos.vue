c98a05cc8ad64b0e2af6bbccc5571be9049d8dee
Alisson Zampietro <alisson.zampietro@gmail.com>
2020-03-10 01:18:33 +0000

    feat: creating todolist

---
+++ src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Todos</h1>
+        <ul v-for="item in todos" v-bind:key="item.id">
+            <li>{{item.title}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Todos',
+    props: [
+        'todos'
+    ]
+}
+</script>
+
+<style scoped>
+
+</style>
